e059fdde0b2b5ba8cd6a6b86850a83603702e871
Kevin Gray <Kevin@GoPosse.com>
2016-07-19 12:34:18 -0400

    Documents the MultipartRequestBody

--- Source/Core/MultipartRequestBody.swift
+++ Source/Core/MultipartRequestBody.swift
@@ -34,16 +34,38 @@
 
 import Foundation
 
+/**
+ A RequestBody class used for Multipart Requests.
+ 
+ - important: It is critically important to call build() on your RequestBody before
+     using it in an Http Request. Failing to do so will almost always result in a crash.
+ */
 public class MultipartRequestBody : RequestBody {
   
+  /**
+   An inner struct of the MultipartRequestBody used to help define MIME types for different
+     types of files.
+   */
   public struct MimeType {
+    /// MIME type for a PNG image.
     public static let ImagePng: String   = "image/png"
+    
+    /// MIME type for a JPEG image
     public static let ImageJpeg: String  = "image/jpeg"
+    
+    /// MIME type for a progressive JPEG (PJPEG) image
     public static let ImageJpegP: String = "image/pjpeg"
+    
+    /// MIME type for a GIF image.
     public static let ImageGif: String   = "image/gif"
+    
+    /// MIME type for a BMP image.
     public static let ImageBmp: String   = "image/bmp"
+    
+    /// MIME type for a TIFF image.
     public static let ImageTiff: String  = "image/tiff"
 
+    /// A map of extensions for the differnt MIME types.
     private static let defaultExts = [
       ImagePng :   "png",
       ImageJpeg :  "jpeg",
@@ -53,6 +75,14 @@ public class MultipartRequestBody : RequestBody {
       ImageTiff :  "tiff",
     ]
     
+    /**
+     Uses the defaultExts property to map a MIME type to an extension.
+     
+     - parameter mimeType: The MIME type to find the default extension of.
+     
+     - returns: The default extension of the mimeType that has been passed in.  If
+         no match is found, "file" is returned.
+     */
     public static func defaultExtension(mimeType: String?) -> String {
       var ext: String = "file"
       if mimeType != nil {
@@ -64,19 +94,44 @@ public class MultipartRequestBody : RequestBody {
     }
   }
   
+  /**
+   A BodyValue type that is used in RequestBody objects of a multipart request.
+   */
   private class Part : BodyValue {
+    
+    /// Static value of the Part class, set to "Content-Disposition"
     static let HeaderContentDisposition = "Content-Disposition"
+    
+    /// Static value of the Part class, set to "Content-Type"
     static let HeaderContentType = "Content-Type"
     
+    /// The name of the file if there is one. Used when creating the contentDisposition.
     var fileName: String?
+    /// The MIME type of the file if there is one. Used when creating the headers for the 
+    /// this Part.
     var mimeType: String?
     
+    /**
+     Initializes a Part object with given values.
+     
+     - parameter name: The key of the BodyValue.
+     - parameter data: The data of the BodyValue.
+     - parameter fileName: The file name of the data being used.
+     - parameter mimeType: The MIME type of the data being used.
+     */
     init(name: String, data: NSData, fileName: String?, mimeType: String?) {
       super.init(name: name, value: data)
       self.fileName = fileName
       self.mimeType = mimeType
     }
     
+    /**
+     Creates a content disposition for this Part object.
+     
+     - returns: The content dispoition, which looks like it uses the name property
+         and the fileName property to create a string like "form-data; name=\\(name); fileName=\\(fileName)".
+         If there is no name, it uses untitled.  If there is no fileName, the fileName part is not included.
+     */
     func contentDisposition() -> String {
       let name: String = self.name ?? "untitled"
       var dispositionValue: String = "form-data; name=\"\(name)\""
@@ -87,6 +142,12 @@ public class MultipartRequestBody : RequestBody {
       return dispositionValue
     }
     
+    /**
+     Generates headers for this Part object.
+     
+     - returns: A dictionary with the content disposition and the MIME type of it is 
+         available.  Uses Part.HeaderContentDisposition and Part.HeaderContentType as key values.
+     */
     func partHeaders() -> [String : String] {
       var headers: [String : String] = [String : String]()
       headers[Part.HeaderContentDisposition] = contentDisposition()
@@ -97,39 +158,96 @@ public class MultipartRequestBody : RequestBody {
     }
   }
   
+  /// The boundary value of the MultipartRequestBody.  Defaulted in the initializer to
+  /// "Boundary+\(arc4random())\(arc4random())"
   public var boundary: String!
+  /// The boundary carriage returns and line feed value. Defaults to "\r\n"
   public var boundaryCRLF = "\r\n"
   
   // MARK: - Initialization
+  
+  /**
+   Initializes a MultipartRequestBody with the default initializer of RequestBody and also
+     sets the boundary property to "Boundary+\(arc4random())\(arc4random())".
+   */
   public override init() {
     super.init()
     self.boundary = "Boundary+\(arc4random())\(arc4random())"
   }
 
+  /**
+   Adds a Part object to the values property.  The Part object is created with the given data.
+   
+   - note: This is the same as calling 
+       addFilePart(fileData: fileData, name: name, fileName: "form_file", mimeType: nil)
+   
+   - parameter fileData: The data to create the Part object with.
+   - parameter name: The key of the Part, which is a BodyValue, that will be created.
+   */
   public func addFilePart(fileData: NSData, name: String) {
     self.values.append(Part(name: name, data: fileData, fileName: "form_file", mimeType: nil))
   }
   
+  /**
+   Adds a Part object to the values property.  The Part object is created with the given data.
+   
+   - parameter fileData: The data to create the Part object with.
+   - parameter name: The key of the Part, which is a BodyValue, that will be created.
+   - parameter fileName: The fileName of the Part object that will be created
+   - parameter mimeType: The mimeType of the Part object that will be created.
+   */
   public func addFilePart(fileData: NSData, name: String, fileName: String, mimeType: String) {
     self.values.append(Part(name: name, data: fileData, fileName: fileName, mimeType: mimeType))
   }
   
   
   // MARK: - Multipart boundaries
+  
+  /**
+   Helper function that generates the initial boundary String.
+   
+   - parameter boundaryString: The boundary String expected, should generally be the
+       boundary parameter
+   
+   - returns: The initial boundary string, i.e. "--\\(boundaryString)\\(boundaryCRLF)"
+   */
   private func boundaryInitial(boundaryString: String) -> String {
     return "--\(boundaryString)\(boundaryCRLF)"
   }
 
+  /**
+   Helper function that generates an inner boundary String.
+   
+   - parameter boundaryString: The boundary String expected, should generally be the 
+       boundary parameter
+   
+   - returns: The inner boundary string, i.e. "\\(boundaryCRLF)--\\(boundaryString)\\(boundaryCRLF)"
+   */
   private func boundaryInner(boundaryString: String) -> String {
     return "\(boundaryCRLF)--\(boundaryString)\(boundaryCRLF)"
   }
 
+  /**
+   Helper function that generates the final boundary String.
+   
+   - parameter boundaryString: The boundary String expected, should generally be the
+       boundary parameter
+   
+   - returns: The final boundary string, i.e. "\\(boundaryCRLF)--\\(boundaryString)--\\(boundaryCRLF)"
+   */
   private func boundaryFinal(boundaryString: String) -> String {
     return "\(boundaryCRLF)--\(boundaryString)--\(boundaryCRLF)"
   }
 
   
   // MARK: - Convert to data
+  
+  /**
+   Override of the generateData function.  Builds the data that can be used in an HTTP request.
+   
+   - returns: The data generated for the MultipartRequestBody.  A whole lot of delimited boundaries and 
+       and data.  I recommend checking out the source directly if you have any questions.
+   */
   public override func generateData() -> NSMutableData {
 
     let initialBoundary: String = boundaryInitial(self.boundary)
@@ -172,6 +290,15 @@ public class MultipartRequestBody : RequestBody {
   }
   
   // MARK: - Multipart headers
+  
+  /**
+   Override of the bodyHeaders function.  Check the return description for more information.
+   
+   - returns: A dictionary of the Content-Type and the Content-Length of the MultipartRequestBody,
+       I.e. following dictionary values:
+       - "Content-Type" : "multipart/form-data; boundary=\(self.boundary)"
+       - "Content-Length" : "\(self.contentLength)"
+   */
   public override func bodyHeaders() -> [String : String] {
     return [
       "Content-Type" : "multipart/form-data; boundary=\(self.boundary)",
