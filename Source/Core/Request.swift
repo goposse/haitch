50bf86af7a51ddb87add54a1cd6255b93f460eae
Kevin Gray <Kevin@GoPosse.com>
2016-07-19 11:18:35 -0400

    Documents the Request.swift file

--- Source/Core/Request.swift
+++ Source/Core/Request.swift
@@ -144,6 +144,12 @@ public class Request {
   /// The HTTP configuration of the request.
   private(set) public var httpClientConfiguration: HttpClientConfiguration?
   
+  /**
+   Initializes a Request with a Builder object.  All properties of this Request
+     are assigned from the Builder.
+   
+   - parameter builder: The Builder object to build this Request from.
+   */
   public init(builder: Request.Builder) {
     self.url = builder.url
     self.params = builder.params
@@ -153,6 +159,12 @@ public class Request {
     self.httpClientConfiguration = builder.httpClientConfiguration
   }
   
+  /**
+   Generates a Builder object from this Request object.  All properties from this 
+     Request are assigned to the Builder.
+   
+   - returns: A new Builder object built from the properties of this Request object.
+   */
   public func newBuilder() -> Request.Builder {
     return Request.Builder()
       .url(url: self.url, params: self.params)
@@ -162,69 +174,172 @@ public class Request {
       .httpClientConfiguration(self.httpClientConfiguration)
   }
   
+  /**
+   Generates the full URL String of this Request which includes the parameters.
+   
+   - returns: The full URL String of the Request, which is the base URL appended with a
+       query string built from any parameters that have been added to it.
+   */
   public func fullUrlString() -> String {
     return NetHelper.urlWithParams(self.url, params: self.params)
   }
 
   // MARK: - Request builder object
+  
+  /**
+   A Builder class for the Request object.  Follows the builder pattern to allow for
+     easy construction and readability of Request objects.
+   */
   public class Builder {
     
+    /// The base URL of the request.
     internal var url: String!
+    
+    /// Query parameters added to the URL.
+    ///
+    /// - note: These are not passed into the body, they are formatted and appended
+    ///   to the URL.
     internal var params: RequestParams = RequestParams()
+    
+    /// The method used for the request, e.g. GET, POST, etc.  Default is GET.
+    /// - seealso: The Method struct within HttpClient.swift.
     internal var method: String = "GET"
+    
+    /// The headers of the HTTP request.
     internal var headers: [String : String] = [String : String]()
+    
+    /// The body of the HTTP request.
     internal var body: RequestBody?
+    
+    /// The HTTP configuration of the request.
     internal var httpClientConfiguration: HttpClientConfiguration?
     
+    /**
+     Default initializer for the Request.Builder.  All properties are set to
+       their default values.  
+     
+     - note: You should not rely on the default values. Build the Request exactly as you need it.
+     */
     public init() {
     }
     
+    /**
+     Sets this Builder's url property and returns it.
+     
+     - parameter url: The URL to assign to this Builder.
+     
+     - returns: This Builder with the new URL assigned to it.
+     */
     public func url(url: String) -> Request.Builder {
       self.url = url
       return self
     }
 
+    /**
+     Sets this Builder's url and params property and returns it.
+     
+     - parameter url: The URL to assign to this Builder.
+     - parameter params: The params to assign to this Builder.
+     
+     - returns: This Builder with the new URL and params assigned to it.
+     */
     public func url(url url: String, params: RequestParams) -> Request.Builder {
       self.url = url
       self.params = params
       return self
     }
 
+    /**
+     Sets this Builder's params property and returns it.
+     
+     - parameter params: The params to assign to this Builder.
+     
+     - returns: This Builder with the new params assigned to it.
+     */
     public func params(params params: RequestParams) -> Request.Builder {
       self.params = params
       return self
     }
     
+    /**
+     Adds a parameter to this Builder's params property and returns it.
+     
+     - parameter name: The key of the parameter to add.
+     - parameter value: The value of the parameter to add
+     
+     - returns: This Builder with the new param added to it.
+     */
     public func addParam(name name: String, value: String) -> Request.Builder {
       self.params.append(name: name, value: value)
       return self
     }
     
+    /**
+     Sets the method property of this Builder and returns it.
+     
+     - parameter method: The HTTP method to assign to this Builder.
+     
+     - returns: This Builder with the new HTTP method assigned to it.
+     */
     public func method(method: String) -> Request.Builder {
       self.method = method
       return self
     }
 
+    /**
+     Sets the headers property of this Builder and returns it.
+     
+     - parameter headers: The headers to assign to this Builder.
+     
+     - returns: This Builder with the new headers assigned to it.
+     */
     public func headers(headers: [String : String]) -> Request.Builder {
       self.headers = headers
       return self
     }
     
+    /**
+     Adds a key/value pair to the headers property of this Builder and returns it.
+     
+     - parameter key: The key of the header to add.
+     - parameter value: The value of the header to add.
+     
+     - returns: This Builder with the new header added to it.
+     */
     public func updateHeader(key key: String, value: String) -> Request.Builder {
       self.headers.updateValue(value, forKey: key)
       return self
     }
 
+    /**
+     Sets the body property of this Builder and returns it.
+     
+     - parameter body: The body to assign to this Builder.
+     
+     - returns: This Builder with the new body assigned to it.
+     */
     public func body(body: RequestBody?) -> Request.Builder {
       self.body = body
       return self
     }
 
+    /**
+     Sets the httpClientConfiguration property of this Builder and returns it.
+     
+     - parameter clientConfig: The HttpClientConfiguration to assign to this Builder.
+     
+     - returns: This Builder with the new configuration assigned to it.
+     */
     public func httpClientConfiguration(clientConfig: HttpClientConfiguration?) -> Request.Builder {
       self.httpClientConfiguration = clientConfig
       return self
     }
 
+    /**
+     Builds a Request object to be used in an HTTP request.
+     
+     - returns: A Request object built from the properties of this Builder object.
+     */
     public func build() -> Request {
       return Request(builder: self)
     }
