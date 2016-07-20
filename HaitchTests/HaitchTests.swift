68777f28b97d91cdb2d07832f9e594b802c64e3a
Kevin Gray <Kevin@GoPosse.com>
2016-07-20 15:19:54 -0400

    Added a test project

---
+++ HaitchTests/HaitchTests.swift
@@ -0,0 +1,27 @@
+//
+//  HaitchTests.swift
+//  HaitchTests
+//
+//  Created by Kevin Gray on 7/20/16.
+//  Copyright © 2016 Posse Productions LLC. All rights reserved.
+//
+
+import XCTest
+
+class HaitchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
